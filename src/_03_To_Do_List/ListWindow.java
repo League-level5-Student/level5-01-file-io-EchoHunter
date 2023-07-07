1de0df72ba24771acfcf57bcb1717f6bb47f6e67
league <league@imac.attlocal.net>
2023-07-06 18:29:51 -0700

    :b:

--- src/_03_To_Do_List/ListWindow.java
+++ src/_03_To_Do_List/ListWindow.java
@@ -2,9 +2,14 @@ package _03_To_Do_List;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ListWindow implements ActionListener {
@@ -15,6 +20,7 @@ JButton viewTask = new JButton();
 JButton removeTask = new JButton();
 JButton saveList = new JButton();
 JButton loadList = new JButton();
+ArrayList<String> list;
 
 public void run() {
 	panel.add(addTask);
@@ -23,6 +29,7 @@ public void run() {
 	panel.add(saveList);
 	panel.add(loadList);
 	frame.add(panel);
+	list = new ArrayList<String>();
 	addTask.setText("Add Task");
 	viewTask.setText("View Task");
 	removeTask.setText("Remove Task");
@@ -39,6 +46,36 @@ public void run() {
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
-	
+	String input = "";
+	if(arg0.getSource() == addTask) {
+		input = JOptionPane.showInputDialog("Input what task to add");
+		list.add(input);
+		JOptionPane.showMessageDialog(null, "Task Added");
+	}else if(arg0.getSource() == viewTask) {
+		JOptionPane.showMessageDialog(null, "Task List: " + list);
+	}else if(arg0.getSource() == removeTask) {
+		input = JOptionPane.showInputDialog("Which task would you like to remove?: " + list);
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).equals(input)) {
+				list.remove(i);
+			}
+		}
+		JOptionPane.showMessageDialog(null, "Task Removed");
+	}else if(arg0.getSource() == saveList) {
+		try {
+			FileWriter wr = new FileWriter("/Level5-Module1/src/_03_To_Do_List/saveText",true);
+			wr.append("/n" + list);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		JOptionPane.showMessageDialog(null, "List Saved");
+	}else if(arg0.getSource() == loadList) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("/Level5-Module1/src/_03_To_Do_List/saveText"));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		JOptionPane.showMessageDialog(null, "List Loaded");
+	}
 }
 }
