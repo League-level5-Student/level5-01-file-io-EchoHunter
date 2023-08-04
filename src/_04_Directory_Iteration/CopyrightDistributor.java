e22fa568f77b9ce0c0bbddb1da02b11ab50a7969
league <maddoxseul@sbsd.net>
2023-08-03 18:37:23 -0700

    ffff

--- src/_04_Directory_Iteration/CopyrightDistributor.java
+++ src/_04_Directory_Iteration/CopyrightDistributor.java
@@ -1,11 +1,27 @@
 package _04_Directory_Iteration;
 
-import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileWriter;
+
 
 public class CopyrightDistributor {
 	public static void main(String[] args) {
-	JFileChooser jfc = new JFileChooser();
-	jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-	jfc.
+	File source = new File("src");
+	recursiveWriter(source.listFiles());
+	}
+	private static void recursiveWriter(File[] inputFile) {
+//		for (File newFile :inputFile) {
+			if (new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").listFiles()!= null) {
+				recursiveWriter(new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").listFiles());
+			}else if(new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").getPath().contains(".txt")) {
+				try {
+					FileWriter fw = new FileWriter(new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").getPath(),true);
+					fw.write("//Copyright © 2023 Edwin Hunter");
+					fw.close();
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		//}
 	}
 }
