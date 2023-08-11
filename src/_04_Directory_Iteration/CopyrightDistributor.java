bf59510bd9e77d12f18bb7e2a4931d47a6a707e3
Student <league@iMac-24.attlocal.net>
2023-08-10 17:08:17 -0700

    backup
    in case the test of this deletes literally everything

--- src/_04_Directory_Iteration/CopyrightDistributor.java
+++ src/_04_Directory_Iteration/CopyrightDistributor.java
@@ -10,18 +10,18 @@ public class CopyrightDistributor {
 	recursiveWriter(source.listFiles());
 	}
 	private static void recursiveWriter(File[] inputFile) {
-//		for (File newFile :inputFile) {
-			if (new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").listFiles()!= null) {
-				recursiveWriter(new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").listFiles());
-			}else if(new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").getPath().contains(".txt")) {
+		for (File newFile :inputFile) {
+			if (newFile.listFiles()!= null) {
+				recursiveWriter(newFile.listFiles());
+			}else if(newFile.getPath().contains(".txt")) {
 				try {
-					FileWriter fw = new FileWriter(new File ("src/_00_Intro_To_File_Input_and_Output/test.txt").getPath(),true);
-					fw.write("//Copyright © 2023 Edwin Hunter");
+					FileWriter fw = new FileWriter(newFile.getPath(),true);
+					fw.write("\n//Copyright © 2023 Edwin Hunter");
 					fw.close();
 				}catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		//}
+		}
 	}
 }
